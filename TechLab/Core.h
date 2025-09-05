148c44f350b8909462316a291c42dc64b1669775
Donghee <luz0415@naver.com>
2025-09-05 18:44:38 +0900

    Add | Core 헤더 파일 추가
    - 언리얼 엔진에서 사용하는 형태로 using, typedef 선언시 이 헤더에 추가

---
+++ TechLab/Core.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// Containers
+#include <vector>
+template<typename T>
+using TArray = std::vector<T>;
+
+
+// Typedef
+typedef int int32;
+typedef unsigned int uint32;
--- TechLab/Object.h
+++ TechLab/Object.h
@@ -1,5 +1,8 @@
 #pragma once
+#include "Core.h"
 class UObject
 {
+public:
+	uint32 UUID;
+	uint32 IntertnalIndex;
 };
-
